6879feb13f70bb7ae92511dcc49dec7837d307c4
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2018-01-15 12:15:39 +0700

    Add some more examples

--- lib/Perinci/Examples/SahSchemas.pm
+++ lib/Perinci/Examples/SahSchemas.pm
@@ -30,5 +30,84 @@ sub schema_perl_modname {
     [200, "OK", \%args];
 }
 
+$SPEC{schema_perl_distname} = {
+    v => 1.1,
+    args => {
+        mod => {
+            schema => 'perl::distname*',
+            req => 1,
+            pos => 0,
+        },
+    },
+};
+sub schema_perl_distname {
+    my %args = @_;
+    [200, "OK", \%args];
+}
+
+$SPEC{schema_filename} = {
+    v => 1.1,
+    args => {
+        mod => {
+            schema => 'filename*',
+            req => 1,
+            pos => 0,
+        },
+    },
+};
+sub schema_filename {
+    my %args = @_;
+    [200, "OK", \%args];
+}
+
+$SPEC{schema_dirname} = {
+    v => 1.1,
+    args => {
+        mod => {
+            schema => 'dirname*',
+            req => 1,
+            pos => 0,
+        },
+    },
+};
+sub schema_dirname {
+    my %args = @_;
+    [200, "OK", \%args];
+}
+
+$SPEC{schema_pathname} = {
+    v => 1.1,
+    args => {
+        mod => {
+            schema => 'pathname*',
+            req => 1,
+            pos => 0,
+        },
+    },
+};
+sub schema_pathname {
+    my %args = @_;
+    [200, "OK", \%args];
+}
+
+$SPEC{schema_date} = {
+    v => 1.1,
+    args => {
+        datetime => {
+            schema => ['date*', 'x.perl.coerce_to' => 'DateTime'],
+        },
+        epoch => {
+            schema => ['date*', 'x.perl.coerce_to' => 'float(epoch)'],
+        },
+        time_moment => {
+            schema => ['date*', 'x.perl.coerce_to' => 'Time::Moment'],
+        },
+    },
+};
+sub schema_date {
+    my %args = @_;
+    [200, "OK", \%args];
+}
+
 1;
 # ABSTRACT:
